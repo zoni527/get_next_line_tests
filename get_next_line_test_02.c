e20cfb740b87d6bad72769d15cbf8a0f918c5cdd
Johnny Varila <jvarila@student.hive.fi>
2024-11-25 18:35:46 +0200

    Fixed an error with test 02, which was overridden by accident.

--- get_next_line_test_02.c
+++ get_next_line_test_02.c
@@ -1,1 +1,36 @@
-asdf
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line_test_02.c                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jvarila <jvarila@student.hive.fi>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/11/21 13:42:01 by jvarila           #+#    #+#             */
+/*   Updated: 2024/11/22 10:08:10 by jvarila          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "get_next_line.h"
+#include <stdio.h>
+#include <fcntl.h>
+
+int	main(int argc, char **argv)
+{
+	(void)argc;
+	(void)argv;
+	char	*rval;
+	int		fd;
+
+	(void)fd;
+	// fd = open("./test_file_06", O_RDONLY);
+	printf("%20s%d\n", "BUFFER_SIZE: ", BUFFER_SIZE);
+	printf("%20s%d\n", "FILE_LIMIT: ", FILE_LIMIT);
+	while((rval = get_next_line(STDIN_FILENO)))
+	{
+		printf("%s", rval);
+		free (rval);
+	}
+	printf("%20s%d\n", "BUFFER_SIZE: ", BUFFER_SIZE);
+	printf("%20s%d\n", "FILE_LIMIT: ", FILE_LIMIT);
+	return (0);
+}
