5d365f5f1812a08b7a45ece151ae9c55d728c1f0
Johnny Varila <jvarila@student.hive.fi>
2024-11-25 18:31:10 +0200

    Removed executables from git repo, updated some testers and shell script files.

--- get_next_line_test_01.sh
+++ get_next_line_test_01.sh
@@ -1,5 +1,1 @@
-#!/bin/bash
-./get_next_line_test_01 < ./test_file_01 > test_file_01_output
-./get_next_line_test_01 < ./test_file_02 > test_file_02_output
-./get_next_line_test_01 < ./test_file_03 > test_file_03_output
-./get_next_line_test_01 < ./test_file_empty > test_file_empty_output
+asdf
