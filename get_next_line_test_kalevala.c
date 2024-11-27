3717e785fb4655c33ee34e5633786867a9834f8a
Johnny Varila <jvarila@student.hive.fi>
2024-11-27 13:57:03 +0200

    Revert kalevala test to valid version

---
+++ get_next_line_test_kalevala.c
@@ -0,0 +1,36 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line_test_kalevala.c                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jvarila <jvarila@student.hive.fi>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/11/21 13:42:01 by jvarila           #+#    #+#             */
+/*   Updated: 2024/11/25 11:55:26 by jvarila          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "get_next_line.h"
+#include <stdio.h>
+#include <fcntl.h>
+
+int	main(int argc, char **argv)
+{
+	(void)argc;
+	(void)argv;
+	char	*rval;
+	int		fd;
+
+	(void)fd;
+	fd = open("./kalevala", O_RDONLY);
+	printf("%20s%d\n", "BUFFER_SIZE: ", BUFFER_SIZE);
+	printf("%20s%d\n", "FILE_LIMIT: ", FILE_LIMIT);
+	while((rval = get_next_line(fd)))
+	{
+		printf("%s", rval);
+		free (rval);
+	}
+	printf("%20s%d\n", "BUFFER_SIZE: ", BUFFER_SIZE);
+	printf("%20s%d\n", "FILE_LIMIT: ", FILE_LIMIT);
+	return (0);
+}
